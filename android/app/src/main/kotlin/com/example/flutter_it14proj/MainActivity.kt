fb123d84db08d21da84ac8cfe403b75b7ae3f2d9
Straña Alynne <salockser@gmail.com>
2023-11-20 00:40:19 +0800

    colors and logo added

---
+++ android/app/src/main/kotlin/com/example/flutter_it14proj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_it14proj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const mobileBackgroundColor = Color.fromRGBO(35, 35, 35, 1);
+const primaryGreen = Color.fromRGBO(21, 160, 39, 1);
+const primaryYellow = Color.fromRGBO(241, 219, 17, 1);
+const primaryWhite = Color.fromRGBO(241, 243, 232, 1);
+const primaryRed = Color.fromRGBO(255, 88, 88, 1);
+const primaryGray = Color.fromRGBO(85, 85, 85, 1);
+const secondaryGreen = Color.fromRGBO(46, 55, 47, 1);
