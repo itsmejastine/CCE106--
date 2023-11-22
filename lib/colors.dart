b9828cbe017cf759f41c8b3c00a1774772ccac56
Straña Alynne <salockser@gmail.com>
2023-11-22 19:05:38 +0800

    added colors for gradient

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/painting/gradient.dart';
 
 const mobileBackgroundColor = Color.fromRGBO(35, 35, 35, 1);
 const primaryGreen = Color.fromRGBO(21, 160, 39, 1);
@@ -7,3 +8,7 @@ const primaryWhite = Color.fromRGBO(241, 243, 232, 1);
 const primaryRed = Color.fromRGBO(255, 88, 88, 1);
 const primaryGray = Color.fromRGBO(85, 85, 85, 1);
 const secondaryGreen = Color.fromRGBO(46, 55, 47, 1);
+
+//gradient colors
+const gradientGreen = Color.fromRGBO(21, 160, 39, 1);
+const gradientYellow = Color.fromRGBO(241, 219, 17, 1);
